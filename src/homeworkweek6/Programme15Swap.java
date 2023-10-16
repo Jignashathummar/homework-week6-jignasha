85fb809522a7416736127baee26ddc9c9a804cea
Jignashathummar <jignashakotadiya10@gmail.com>
2023-10-16 19:56:02 +0100

    update programe 15

--- src/homeworkweek6/Programme15Swap.java
+++ src/homeworkweek6/Programme15Swap.java
@@ -1,17 +1,31 @@
 package homeworkweek6;
 
+import java.util.Scanner;
+
 /**
  * Java program to swap two variables.
  */
 public class Programme15Swap {
     public static void main(String[] args) {
-        int a, b, c;
-        a = 15;
-        b = 21;
-        System.out.println("Before swapping: a, b = " + a + " , " + b);
-        c = a;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a: ");
+        int a = sc.nextInt();
+        System.out.println("Please enter b: ");
+        int b = sc.nextInt();
+        swapWithoutThirdVariable(a,b);
+        swapWithThirdVariable(a, b);
+
+    }
+    public static void swapWithThirdVariable(int a, int b){
+        int c = a;
         a = b;
         b = c;
-        System.out.println("After swapping: a, b = " + a + " , " + b);
+        System.out.println("Swap value with using third variable: a = " + a + ", b = " + b);
+    }
+    public static void swapWithoutThirdVariable(int a, int b){
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("Swap value without using third variable: a = " + a + ", b = " + b);
     }
 }

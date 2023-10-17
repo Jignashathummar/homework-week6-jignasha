9d7837a748af1593d3a5788c37df2f2398a14cd7
Jignashathummar <jignashakotadiya10@gmail.com>
2023-10-17 19:56:36 +0100

    better use logic in programme 10

--- src/homeworkweek6/Programme10forTable.java
+++ src/homeworkweek6/Programme10forTable.java
@@ -8,12 +8,14 @@ import java.util.Scanner;
 public class Programme10forTable {
 
     //print statement for table
+    //method 1
     public static void printTable(int num, int i) {
         System.out.println(num + " * " + i + " = " + num * i);
     }
 
     // logic for creating table method
     public static void table(int num) {
+
         printTable(num, 1);
         printTable(num, 2);
         printTable(num, 3);
@@ -30,7 +32,24 @@ public class Programme10forTable {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number: ");
         int num = scanner.nextInt();
+
+        System.out.println("Create table using method 1 like ten time print statment");
         table(num);
+
+        System.out.println("Create table using method 2 look like for loop but hasn't used for loop");
+        int start = 1;
+        int end = 10;
+        printNumberTable(start, end, num);
         scanner.close();
     }
+//method 2
+    public static void printNumberTable(int start, int end, int num) {
+        if (start > end) {
+            return;
+        }
+
+        System.out.println(num + " * " + start + " = " + start * num);
+        printNumberTable(start + 1, end, num);
+    }
+
 }
